2c9a5865c5661ebe14db97b2012a29bb36dc562b
KASTURI SRAVYA <kasturisravya15@gmail.com>
2025-01-28 16:10:32 +0530

    Add files via upload

---
+++ Powerm.java
@@ -0,0 +1,10 @@
+public class Powerm{
+        public static void main(String[] args){
+            double a=2;
+            double b=5;
+            System.out.println("Math.pow("+a+","+b+")"+Math.pow(a,b));
+            System.out.println("Math.pow("+b+","+a+")"+Math.pow(b,a));
+
+       }
+    }
+
